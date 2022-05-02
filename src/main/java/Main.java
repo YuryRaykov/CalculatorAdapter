939871c5ed5121cfb286c649ff123038a98c02ec
Юрий Райков <Therion988@yandex.ru>
2022-05-02 13:35:45 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Ints calc  = new IntsCalculator();
+        // демонстрация
+        System.out.println(calc.mult(2, 2));
+        System.out.println(calc.sum(10, 20));
+        System.out.println(calc.pow(2,10));
+    }
+}
